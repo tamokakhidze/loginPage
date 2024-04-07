b995536e32d0a118e71a60c06ffa719a134fe2a7
Tamuna Kakhidze <tamuna.kakhidze.1@btu.edu.ge>
2024-04-07 23:19:56 +0400

    deleted storyboard

--- loginPage/loginPage/ViewController.swift
+++ loginPage/loginPage/ViewController.swift
@@ -9,6 +9,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         createMainStackView()
         createLockImage()
         createWelcomeStackView()
